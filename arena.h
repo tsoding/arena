b81d36a73bf53c56f709fc96f84e3fcfbf136ac3
nic@bananya <nic@bananya>
2025-04-20 07:54:06 -0300

    Add `arena_memcpy` to arena header

--- arena.h
+++ arena.h
@@ -73,6 +73,7 @@ void *arena_alloc(Arena *a, size_t size_bytes);
 void *arena_realloc(Arena *a, void *oldptr, size_t oldsz, size_t newsz);
 char *arena_strdup(Arena *a, const char *cstr);
 void *arena_memdup(Arena *a, void *data, size_t size);
+void *arena_memcpy(void *dest, const void *src, size_t n);
 #ifndef ARENA_NOSTDIO
 char *arena_sprintf(Arena *a, const char *format, ...);
 #endif // ARENA_NOSTDIO
